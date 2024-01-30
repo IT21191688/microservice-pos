8dec932a1531bad7668d1732d99613cf2c409c83
IT21191688 <IT21191688@my.sliit.lk>
2024-01-30 19:16:24 +0530

    api gateway

--- api-gateway/src/main/java/com/shop/apigateway/filter/CustomFilter.java
+++ api-gateway/src/main/java/com/shop/apigateway/filter/CustomFilter.java
@@ -1,2 +1,24 @@
-package com.shop.apigateway.filter;public class CustomFilter {
+package com.shop.apigateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class CustomFilter implements GlobalFilter {
+
+    //pre filter
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //security
+        ServerHttpRequest serverHttpRequest=exchange.getRequest();
+
+        System.out.println("Auth :"+serverHttpRequest.getHeaders().getFirst("Authorization"));
+
+
+        return chain.filter(exchange);
+    }
 }
--- customer-service-api/src/main/java/com/shop/customerserviceapi/dto/OrderDto.java
+++ customer-service-api/src/main/java/com/shop/customerserviceapi/dto/OrderDto.java
@@ -1,6 +1,5 @@
 package com.shop.customerserviceapi.dto;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
